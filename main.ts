51ed2a151e59035de3188277ce71384a1ea2f63d
CodingRobIT <tmai02476@gmail.com>
2024-09-22 16:08:51 +0200

    Nele neu Programm

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    if (ledAus1) {
-        ledAus1 = false
-        ledAus2 = true
-        music.playTone(262, music.beat(BeatFraction.Whole))
+    if (LEDvonKnopfAAusist) {
+        // LED an auf false setzen funktioniert nicht weil dann im Loop das nicht erkannt wird warum auch immer das so ist
+        LEDvonKnopfAAusist = false
+        LEDvonKnopfBAusist = true
         basic.showLeds(`
             . # . # .
             # # # # #
@@ -11,8 +11,8 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
             . . # . .
             `)
     } else {
-        ledAus1 = true
-        music.playTone(330, music.beat(BeatFraction.Whole))
+        // LED an auf false setzen funktioniert nicht weil dann im Loop das nicht erkannt wird warum auch immer das so ist
+        LEDvonKnopfAAusist = true
         basic.showLeds(`
             . . . . .
             . . . . .
@@ -23,22 +23,22 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     }
 })
 input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
-    ledAus2 = true
-    ledAus1 = true
+    LEDvonKnopfBAusist = true
+    LEDvonKnopfAAusist = true
     if (input.isGesture(Gesture.LogoUp)) {
         basic.showLeds(`
             . . # . .
-            . # # # .
-            # . # . #
             . . # . .
+            # . # . #
+            . # # # .
             . . # . .
             `)
     } else if (input.isGesture(Gesture.LogoDown)) {
         basic.showLeds(`
             . . # . .
-            . . # . .
-            # . # . #
             . # # # .
+            # . # . #
+            . . # . .
             . . # . .
             `)
     } else if (input.isGesture(Gesture.TiltLeft)) {
@@ -68,10 +68,9 @@ input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
     }
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-    if (ledAus2) {
-        ledAus2 = false
-        ledAus1 = true
-        music.playTone(262, music.beat(BeatFraction.Whole))
+    if (LEDvonKnopfBAusist) {
+        LEDvonKnopfBAusist = false
+        LEDvonKnopfAAusist = true
         basic.showLeds(`
             . . . . .
             . # . # .
@@ -79,9 +78,9 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
             # . . . #
             . # # # .
             `)
+        music.playTone(262, music.beat(BeatFraction.Whole))
     } else {
-        ledAus2 = true
-        music.playTone(330, music.beat(BeatFraction.Whole))
+        LEDvonKnopfBAusist = true
         basic.showLeds(`
             . . . . .
             . . . . .
@@ -89,14 +88,15 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
             . . . . .
             . . . . .
             `)
+        music.playMelody("E E F G G F E D ", 120)
+        music.playMelody("C C D E E D D - ", 120)
     }
 })
-let ledAus2 = false
-let ledAus1 = false
+let LEDvonKnopfAAusist = false
+let LEDvonKnopfBAusist = false
+LEDvonKnopfBAusist = true
 // LED an auf false setzen funktioniert nicht weil dann im Loop das nicht erkannt wird warum auch immer das so ist
-ledAus1 = true
-ledAus2 = true
-let sonderVariable = 0
+LEDvonKnopfAAusist = true
 while (true) {
     basic.setLedColor(0x0000ff)
     basic.pause(2000)
